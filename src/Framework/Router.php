ce268ab028c51e8032dfef362c474c871059235a
polinkesDev <polinkesdev@gmail.com>
2024-05-28 19:23:12 +0700

    Add a simple Router normalization method

--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -9,9 +9,18 @@ class Router
     private array $routes = [];
 
     public function add(string $method, string $path) {
+        $path = $this->normalizePath($path);
+
         $this->routes[] = [
             'path' => $path,
             'method'=> strtoupper($method)
         ];
+    }
+
+    private function normalizePath(string $path): string
+    {
+        $path = trim($path, '/');
+        $path = "/{$path}/";
+        return $path;
     }
 }
