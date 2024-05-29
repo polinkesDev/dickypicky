5afd920e2e5cf0c70d5dabcf837062faaac2d8b1
polinkesDev <polinkesdev@gmail.com>
2024-05-29 18:56:41 +0700

    Create Controllers dir and HomeController class

---
+++ src/App/Controllers/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+class HomeController {
+    public function home() // or 'index' or 'root'
+    {
+        echo 'Home Page 0';
+    }
+}
