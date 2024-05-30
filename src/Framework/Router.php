d27a7781187c9d48cd07e27efb322781c0f1f727
polinkesDev <polinkesdev@gmail.com>
2024-05-30 11:45:41 +0700

    Instantiate a controller
    Instantiate a controller via strings

--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -41,7 +41,9 @@ class Router
                 continue;
             }
 
-            echo 'route found';
+            [$class, $function] = $route['controller'];
+            $controllerInstance = new $class;
+            $controllerInstance->$function(); // or $controllerInstance->{$function}();
         }
     }
 }
