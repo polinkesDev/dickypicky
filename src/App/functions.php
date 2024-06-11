8d7b2663245298a0459f75cc9ffc55e3213b1fcb
polinkesDev <polinkesdev@gmail.com>
2024-06-11 17:15:30 +0700

    Add parameters validation in a Container resolve method
    Using ReflectionNamedType and ContainerException

--- src/App/functions.php
+++ src/App/functions.php
@@ -6,7 +6,7 @@ function dd(mixed $value) { // "dump-and-die" function
     echo "<pre>";
         print_r($value);
     echo "</pre>";
-    //die(); // stop load the page
+    die(); // stop load the page
 }
 
 function esc(mixed $value): string
--- src/Framework/Container.php
+++ src/Framework/Container.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace Framework;
 
-use ReflectionClass;
+use ReflectionClass, ReflectionNamedType;
 use Framework\Exceptions\ContainerException;
 
 class Container
@@ -33,6 +33,21 @@ class Container
 
         if (count($params) === 0) {
             return new $className;
+        }
+
+        $dependencies = [];
+
+        foreach ($params as $param) {
+            $name = $param->getName();
+            $type = $param->getType();
+
+            if (!$type) {
+                throw new ContainerException("Failed to resolve class {$className} because param {$name} is missing a type hint.");
+            }
+
+            if (!$type instanceof ReflectionNamedType  || $type->isBuiltin()) {
+                throw new ContainerException("Failed to resolve class {$className} because invalid param name.");
+            }
         }
 
         dd($params);
