bbece2d69adf3fb71183264ab083a239c7bd55af
polinkesDev <polinkesdev@gmail.com>
2024-06-11 15:06:21 +0700

    Check if constructor has parameters

--- src/Framework/Container.php
+++ src/Framework/Container.php
@@ -29,6 +29,12 @@ class Container
             return new $className;
         }
 
-        dd($constructor);
+        $params = $constructor->getParameters();
+
+        if (count($params) === 0) {
+            return new $className;
+        }
+
+        dd($params);
     }
 }
